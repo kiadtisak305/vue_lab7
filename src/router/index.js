bb4e41065cc6e071a35e95fedc88e5a91c085fbc
kiadtisak305 <kiadtisak.saesong123@gmail.com>
2023-02-10 21:43:16 +0700

    updatefile

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import NewsView from '../views/NewsView.vue'
 import PlansView from '../views/PlansView.vue'
 import AddDropView from '../views/AddDropView.vue'
-import Comsci from '../components/Com-sci.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -20,11 +19,6 @@ const router = createRouter({
       path: '/adddrop',
       name: 'adddrop',
       component: AddDropView
-    },
-    {
-      path: '/com-sci',
-      name: 'com-sci',
-      component: Comsci
     }
   ]
 })
